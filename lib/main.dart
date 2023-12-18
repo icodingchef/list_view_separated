d1a9ce2c66c770494bbac97fc4bee017363a04bb
icodingchef <icodingchef@gmail.com>
2023-12-18 21:14:40 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:list_view_separated/separated.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ListView.builder Example',
+      theme: ThemeData(
+        colorSchemeSeed: Colors.blue,
+      ),
+      home: Separated(),
+    );
+  }
+}
